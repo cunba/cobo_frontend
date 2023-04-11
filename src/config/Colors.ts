3756237ed50d6b75a237f6ae81326f91f14d7134
cunba <ire.cunba@gmail.com>
2023-04-11 21:46:26 +0200

    add colors pallet

--- src/config/Colors.ts
+++ src/config/Colors.ts
@@ -1,13 +1,29 @@
+const PALLET = {
+    antiflash_white: '#eeeeee',
+    pastel_blue: '#90daff',
+    lemon: '#fcf6bd',
+    pastel_yellow: '#fff07c',
+    pastel_green: '#c1fba4',
+    pastel_pink: '#ffabc1',
+    pink: '#f194b4',
+    lavander: '#e4c1f9',
+    periwinkle: '#bcb6ff',
+    dark_periwinkle: '#b1abf7',
+    lilac: '#8f88dd',
+    lilac_darker: '#867fcf',
+    light_black: '#4D4D4D'
+}
+
 export const COLORS_LIGHT = {
-    background: "white",
-    text: "black",
-    textButtons: "white",
-    touchables: 'blue'
+    background: PALLET.periwinkle,
+    text: 'black',
+    textButtons: 'white',
+    touchables: PALLET.lilac,
 }
 
 export const COLORS_DARK = {
-    background: "black",
-    text: "white",
-    textButtons: "white",
-    touchables: 'blue'
+    background: PALLET.lilac_darker,
+    text: 'white',
+    textButtons: 'black',
+    touchables: PALLET.periwinkle
 }
